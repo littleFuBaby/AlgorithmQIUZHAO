65fc40fc7147b7837e350f2d2689a3f9c863e3dc
andy fu <520fys008@163.com>
2020-07-23 23:58:31 +0800

    n tree previous order

--- Week_02/NTreePreOrder.java
+++ Week_02/NTreePreOrder.java
@@ -1,13 +1,10 @@
 package com.ruoyi.grokking.algorithms.nTree;
 
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
-public class NTreePostOrder {
+public class NTreePreOrder {
 
-    class Node{
-
+    class Node {
         public int val;
         public List<Node> children;
 
@@ -24,54 +21,58 @@ public class NTreePostOrder {
     }
 
     /**
-     * private node val list for postorder method
+     * list for recursive method
      */
     private List<Integer> nodeValList = new LinkedList<>();
 
     /**
-     * method 1
-     * post order of n tree
-     * @param root
+     * method 1 By using recursion
      * @return
      */
-    public List<Integer> postorder1(Node root){
-
-        // note : there exists a bug for this algorithm
+    public List<Integer> preorder1(Node root){
 
         // base case
-        if(root == null){
+        if (root == null){
             return nodeValList;
         }
 
         // recursive case
         for(Node node : root.children){
-            postorder1(node);
+            preorder1(node);
         }
 
-        // post order
+        // previous order
         nodeValList.add(root.val);
         return nodeValList;
     }
 
     /**
-     * method 2
-     * iterator
+     * method 2 By using iterating
+     * @param root
      * @return
      */
-    public List<Integer> postorder2(Node root) {
+    public List<Integer> preorder2(Node root){
+
         LinkedList<Integer> list = new LinkedList<>();
+        // check
         if (root == null){
             return list;
         }
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        queue.add(root);
-        while (!queue.isEmpty()){
-            Node node = queue.pollLast();
-            list.addFirst(node.val);
+
+        ArrayDeque<Node> deque = new ArrayDeque<>();
+        deque.add(root);
+        // iterate
+        while (!deque.isEmpty()){
+            Node node = deque.pollLast();
+            list.add(node.val);
+            // reverse children node
+            Collections.reverse(node.children);
             for(Node n : node.children){
-                queue.addLast(n);
+                deque.add(n);
             }
         }
+
         return list;
     }
+
 }
