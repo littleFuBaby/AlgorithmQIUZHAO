a91185b520d0e05f53c8f94e568db21485e00bc6
andy fu <520fys008@163.com>
2020-07-24 08:28:43 +0800

    update

--- Week_02/BTreeInOrder.java
+++ Week_02/BTreeInOrder.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-public class TwoTreeInOrder {
+public class BTreeInOrder {
 
     public class TreeNode {
         int val;
