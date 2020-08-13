278c39175c266acb54d6ddd0c7cc4aa00c4dd763
andy fu <520fys008@163.com>
2020-08-13 12:20:25 +0800

    anagram

---
+++ Week_01/IsAnagram_ASCII.java
@@ -0,0 +1,20 @@
+package com.ruoyi.grokking.algorithms.hash;
+
+public class IsAnagram_ASCII {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] array = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            array[s.charAt(i) - 'a']++;
+            array[t.charAt(i) - 'a']--;
+        }
+        for (int i : array) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+
+}
---
+++ Week_01/IsAnagram_Sort.java
@@ -0,0 +1,16 @@
+package com.ruoyi.grokking.algorithms.hash;
+
+import java.util.Arrays;
+
+public class IsAnagram_Sort {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
